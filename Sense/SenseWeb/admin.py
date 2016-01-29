f67d108cd8baaa347666876fc74943454473651b
Stanley Chiang <mraaa@bu.edu>
2016-01-30 04:24:53 +0800

    Update admin.py

--- Sense/SenseWeb/admin.py
+++ Sense/SenseWeb/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Movie, Quiz, QuizSet, Answer
+from SenseWeb.models import Movie, Quiz, QuizSet, Answer
 
 # Register your models here.
 admin.site.register(Movie)
